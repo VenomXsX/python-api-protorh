23b707de81500a4a340bae650364c4f6df0d32b3
TheMysteryz <themysteryz@users.noreply.github.com>
2023-10-23 12:09:36 +0200

    draft

--- protorh/database.py
+++ protorh/database.py
@@ -2,21 +2,9 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy import create_engine
 from sqlalchemy_utils import database_exists, create_database
-from dotenv import load_dotenv
-import os
+from env import DB_URL
 
-
-load_dotenv("protorh/protorh.env")
-db_user = os.environ["DATABASE_USER"]
-db_password = os.environ["DATABASE_PASSWORD"]
-db_host = os.environ["DATABASE_HOST"]
-db_port = os.environ["DATABASE_PORT"]
-db_name = os.environ["DATABASE_NAME"]
-
-db_url = "postgresql://" + db_user + ":" + db_password + \
-    "@" + db_host + ":" + db_port + "/" + db_name
-
-engine = create_engine(db_url, echo=True)
+engine = create_engine(DB_URL, echo=True)
 if not database_exists(engine.url):
     create_database(engine.url)
 
